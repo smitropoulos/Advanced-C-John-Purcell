1bf0b884b6ee185cab4b9f2aedc8b747684f1a66
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-03-19 15:11:15 +0200

    Initial Commit

---
+++ Advanced C++ John Purcell/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Advanced C++ John Purcell
+//
+//  Created by Stefanos Mitropoulos on 19/03/2018.
+//  Copyright © 2018 Stefanos Mitropoulos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
