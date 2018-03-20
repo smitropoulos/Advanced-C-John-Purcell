f289288b377a4471fa4c91cc09140678e9c4fc75
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-03-20 10:16:06 +0200

    added commenting

--- Advanced C++ John Purcell/STL/Lists.cpp
+++ Advanced C++ John Purcell/STL/Lists.cpp
@@ -24,13 +24,37 @@ void Lists(){
 	std::list<int>::iterator it;
 
 	it = listOfNumbers.begin();
-	std::cout<<*it<<std::endl;
+	it++;
+	listOfNumbers.insert(it,20);
+	std::cout<<"Element: "<<*it<<std::endl;
 
 
-
-
-	for (std::list<int>::iterator it=listOfNumbers.begin();it!=listOfNumbers.end();it++){
+/*
+	for (std::list<int>::iterator it=listOfNumbers.begin()++;it!=listOfNumbers.end();it++){
 		std::cout<<*it<<std::endl;
 	}
+*/
+
+		//in case you want to delete an element of a list, then the iterator will immediately point to the next element.
+
+	for (std::list<int>::iterator Eraseit=listOfNumbers.begin()++;Eraseit!=listOfNumbers.end();){
+
+		if (*Eraseit == 2){
+			listOfNumbers.insert(Eraseit,23);
+		}
+
+		if (*Eraseit == 1){
+			Eraseit = listOfNumbers.erase(Eraseit);	//The erase method, returns a pointer to the element that would have been next (aka the element that got pushed back to the position the element being erased was)
+		}else{
+			Eraseit++;
+		}
+
+	}
+
+	//"loop" through all the elements of the list.
+	for (std::list<int>::iterator it=listOfNumbers.begin()++;it!=listOfNumbers.end();it++){
+		std::cout<<*it<<std::endl;
+	}
+
 
 }
